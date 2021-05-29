878d79f928025fb173d6bc803a7cac10ad7f1599
Ryan Park <ryan.developer99@gmail.com>
2021-05-29 19:44:22 +0900

    add:useStateObject

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person, setPerson] = useState({
+    name: "john",
+    age: 17,
+    message: "hello world",
+  });
+
+  console.log(person);
+
+  const changeMessage = () => {
+    console.log("clicked");
+    if (person.message === "hello world") {
+      setPerson({ ...person, message: "happy world" });
+    } else {
+      setPerson({ ...person, message: "hello world" });
+    }
+  };
+
+  return (
+    <div>
+      <h2>{person.name}</h2>
+      <h2>{person.age}</h2>
+      <h2>{person.message}</h2>
+      <button className="btn" onClick={changeMessage}>
+        Change Message
+      </button>
+    </div>
+  );
 };
 
 export default UseStateObject;
--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -1,7 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateCounter = () => {
-  return <h2>useState counter example</h2>;
+  console.log("counter");
+
+  let [number, setNumber] = useState(0);
+  console.log(number);
+
+  let decrement = () => {
+    console.log("decrement");
+    setNumber(--number);
+  };
+
+  let increment = () => {
+    console.log("decrement");
+    setNumber(++number);
+  };
+
+  return (
+    <div>
+      <button className="decrement" onClick={decrement}>
+        <h1>-</h1>
+      </button>
+      <h1>{number}</h1>
+      <button className="increment" onClick={increment}>
+        <h1>+</h1>
+      </button>
+    </div>
+  );
 };
 
 export default UseStateCounter;
