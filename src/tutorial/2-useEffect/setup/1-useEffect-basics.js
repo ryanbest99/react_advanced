73fd5223e601b8046ac2d5e6523563c29f5cbc89
Ryan Park <ryan.developer99@gmail.com>
2021-05-31 13:42:28 +0900

    add: useEffect-basics

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -1,9 +1,47 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 // by default runs after every re-render
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+  const [value, setValue] = useState(0);
+
+  const callBack = () => {
+    console.log("call useEffect");
+    if (value > 1) {
+      document.title = `Look ${value}`;
+    }
+  };
+
+  const callBack2 = () => {
+    console.log("call2 useEffect");
+    if (value > 1) {
+      document.title = `Look ${value * 2}`;
+    }
+  };
+
+  useEffect(callBack, []);
+  useEffect(callBack2, [value]);
+
+  console.log("render component");
+
+  // useEffect(() => {
+  //   console.log("call useEffect");
+  //   document.title = `Look ${value}`;
+  // });
+
+  return (
+    <>
+      <h1>{value}</h1>
+      <button
+        className="btn"
+        onClick={() => {
+          setValue(value + 1);
+        }}
+      >
+        Click
+      </button>
+    </>
+  );
 };
 
 export default UseEffectBasics;
