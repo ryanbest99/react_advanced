710c6c6f1b667c6555bebf1c37d47fbf77472ad0
Ryan Park <ryan.developer99@gmail.com>
2021-06-26 14:28:14 +0900

    add: review-customHooks

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 // import Setup from "./tutorial/8-useContext/setup/2-context";
-import Setup from "./tutorial/8-useContext/setup/5-context";
+import Setup from "./tutorial/9-custom-hooks/setup4/";
 
 function App() {
   return (
---
+++ src/tutorial/9-custom-hooks/setup4/index.js
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from "react";
+import { useFetch } from "./fetch";
+
+// ATTENTION!!!!!!!!!!
+// I SWITCHED TO PERMANENT DOMAIN
+const url = "https://course-api.com/javascript-store-products";
+
+const Index = () => {
+  //
+  const { loading, products } = useFetch(url);
+  //
+  return (
+    <div>
+      <h2>{loading ? "loading..." : "data"}</h2>
+    </div>
+  );
+};
+
+export default Index;
